205dbbe7653a0b9899f43fbee908e4e4a1ea2a0f
yuriyaza <yuriy.aza@gmail.com>
2023-09-13 19:28:26 +0300

    Update 23

--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -1,16 +1,15 @@
 export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
+    return (
+        <div
+            style={{
+                height: '100vh',
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center',
+                fontSize: 40,
+                color: '#010101',
+            }}>
+            React homework template
+        </div>
+    );
 };
