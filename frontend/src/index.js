4bb7630276b74df4c40fb2342d1e5a93d9f6f886
yuriyaza <yuriy.aza@gmail.com>
2023-09-13 16:50:37 +0300

    Update 12

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,13 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
 import './index.css';
-// import  { BrowserRouter } from 'react-router-dom'
+import  { BrowserRouter } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    {/* <BrowserRouter basename="/template-front-plus-back"> */}
+    <BrowserRouter basename="/">
       <App />
-    {/* </BrowserRouter> */}
+    </BrowserRouter>
   </React.StrictMode>
 );
 
