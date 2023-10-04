d8a04fb705833cc01f8a171166f1a2f8026e9b2e
yuriyaza <yuriy.aza@gmail.com>
2023-10-04 22:15:22 +0300

    Update 39

--- client/src/index.js
+++ client/src/index.js
@@ -8,7 +8,7 @@ import { BrowserRouter } from 'react-router-dom';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename='/template-front-plus-back'>
+    <BrowserRouter basename='/template-front-plus-back/client'>
       <App />
     </BrowserRouter>
   </React.StrictMode>
